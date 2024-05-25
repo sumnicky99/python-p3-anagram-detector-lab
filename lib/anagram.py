bd71d68181506ee82ab1b8aad3131d511e970168
Sumnicky99 <sumnicky99@gmail.com>
2024-05-25 12:57:27 +0300

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list):
+        possible_anagrams = []
+        
+        word_list = sorted([letter for letter in self.word])
+        for word in list:
+            if sorted([letter for letter in word]) == word_list:
+                possible_anagrams.append(word)
+        return possible_anagrams
